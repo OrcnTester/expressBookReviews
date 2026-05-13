35e5190685dcc5c5dea8ffcb232f3f7b2483e4c9
Orcun YORUK <134277629+OrcnTester@users.noreply.github.com>
2026-05-13 15:33:37 +0300

    Refactor authentication middleware for customer routes

--- final_project/index.js
+++ final_project/index.js
@@ -22,14 +22,21 @@ app.use(
     })
 );
 
-app.use('/customer/auth/*', function auth(req, res, next) {
-    const auth = req.session.authorization;
-    if (auth) {
-        jwt.verify(auth['token'], 'secretOrPrivateKey', (error, decoded) => {
-            if (!error) next();
-            else return res.status(401).json({ message: 'Unauthorized' })
-        })
-    } else return res.status(403).json({ message: 'Forbidden' })
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (req.session.authorization) {
+        const token = req.session.authorization["accessToken"];
+
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next();
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
 
 
